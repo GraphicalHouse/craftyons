85586607a26312cf7b9efc57e82a73f9c0582616
James Greig <3stripe@gmail.com>
2018-03-19 07:19:34 +0000

    Installer requests dev URL, reminds of root db pw

--- installCraftyons.js
+++ installCraftyons.js
@@ -8,13 +8,14 @@ const log = console.log;
 const rule = chalk.blue('\n------------------');
 const cmd = require('node-run-cmd');
 const mysql = require('mysql');
+const spinner = ora('Downloading Craftyons... this will take a few minutes\n');
+
 var dbName;
-
-// NtWZUiMK%HL9aKwNBBj
-
+var dbPassword;
+var devUrl;
 var prompt = require('minimal-prompt');
 
-prompt.question(['Client name', 'Client URL', 'Database name', 'Database password'], {
+prompt.question(['Client name', 'Local dev URL',  'Database name', 'Database password'], {
     prompt: '>',
     delimiter: ':',
     formatPrompt: function(prompt, delim, name) {
@@ -23,7 +24,8 @@ prompt.question(['Client name', 'Client URL', 'Database name', 'Database passwor
     },
     onComplete: function(results) {
       var dbName = results.databaseName;
-      var dbPassword = results.databasePassword;
+      var dbPassword = results.localDevUrl;
+      var devUrl = results.databasePassword;
       var con = mysql.createConnection({
         host: "localhost",
         user: "root",
@@ -36,7 +38,7 @@ prompt.question(['Client name', 'Client URL', 'Database name', 'Database passwor
         con.query("CREATE DATABASE " + dbName);
   			// cmd.run('mysql -u root -p ' + dbName + ' < craftyons/database.sql')
   			log(chalk.blue('Now run the command below to import the Craftyons database into ' + dbName + ':'));
-  			log(chalk.blue('[You will be prompted to enter MySQL\'s root user password again, derp]'));
+  			log(chalk.bgBlue('Note: you will be asked for MySQL\'s root user password again, derp... it\'s: ' + dbPassword));
   			log(chalk.red('mysql -u root -p ' + dbName + ' < database.sql && valet link && valet secure && valet open && gulp'));
       });
       // console.log(chalk.bgCyan('Database name:', results.databaseUsername));
@@ -45,11 +47,21 @@ prompt.question(['Client name', 'Client URL', 'Database name', 'Database passwor
     }
 });
 
-var dataCallback = function(data) {
-  log(data);
-};
-
-
-
-// prompt.start() will begin the prompting process.
+// Start the prompting process.
 prompt.start();
+
+// var dataCallback = function(data) {
+//   log(data);
+// };
+
+// var craftInstallPrompt = function() {
+//   spinner.stop();
+//   log(chalk.blue('Nice one pal, Craftyons has been downloaded\n'));
+//   prompt.start();
+// }
+
+// spinner.start();
+//
+// cmd.run('composer create-project graphicalhouse/craftyons . -s RC',
+// 	{ onDone: craftInstallPrompt }
+// );
